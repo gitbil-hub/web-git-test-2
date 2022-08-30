7320ef706af9655e0beb07e54b811b66868b43da
github-bil <nabillathif2805@gmail.com>
2022-08-30 08:56:01 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
 <body>
     <h1>Hello  World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius molestiae nemo quam asperiores magni modi laboriosam maiores porro molestias autem!</p>
+
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
